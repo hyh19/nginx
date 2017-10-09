9f9b1fb00514a68e5e1b2b8423d1b375e86f6f71
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-09 15:30:15 +0800

    Update and rename install-nginx-ubuntu1204.sh to install-nginx-ubuntu1604.sh

---
+++ install-nginx-ubuntu1604.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget http://nginx.org/keys/nginx_signing.key
+sudo apt-key add nginx_signing.key
+
+echo 'deb http://nginx.org/packages/ubuntu/ xenial nginx' >> /etc/apt/sources.list
+echo 'deb-src http://nginx.org/packages/ubuntu/ xenial nginx' >> /etc/apt/sources.list
+
+apt-get update
+apt-get install nginx -y
