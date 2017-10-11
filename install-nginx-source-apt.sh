c36fa41887c2cb2e5ec29dfdd7e63c3edc3e9410
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-11 11:33:16 +0800

    Update install-nginx-source-apt.sh

--- install-nginx-source-apt.sh
+++ install-nginx-source-apt.sh
@@ -2,7 +2,7 @@
 
 ## Ubuntu 14.04/16.04/17.04, Debian 7.11 ##
 
-# curl -L https://github.com/mrhuangyuhui/nginx-practice/raw/master/install-nginx-source-ubuntu.sh | sh
+# curl -L https://github.com/mrhuangyuhui/nginx-practice/raw/master/install-nginx-source-apt.sh | sh
 
 # 更新软件库
 apt-get update
