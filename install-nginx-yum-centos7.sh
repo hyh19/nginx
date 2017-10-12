a8fe8c4da980976df3e4e1ed057993852652fb4d
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-12 11:01:05 +0800

    Create install-nginx-yum-centos7.sh

---
+++ install-nginx-yum-centos7.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# curl -L https://github.com/mrhuangyuhui/nginx-practice/raw/master/install-nginx-yum-centos7.sh | sh
+
+# 安装下载工具
+yum install -y wget
+
+# 下载 nginx 官方源
+wget -O /etc/yum.repos.d/nginx.repo https://github.com/mrhuangyuhui/nginx-practice/raw/master/nginx-centos7.repo
+
+# 安装 nginx
+yum install -y nginx
+
+# 打印 nginx 版本信息
+nginx -v
