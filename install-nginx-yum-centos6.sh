72e2cd96ecf395b97bc76615f1d885458489d4d0
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-12 10:50:41 +0800

    Update install-nginx-yum-centos6.sh

--- install-nginx-yum-centos6.sh
+++ install-nginx-yum-centos6.sh
@@ -2,6 +2,9 @@
 
 # curl -L https://github.com/mrhuangyuhui/nginx-practice/raw/master/install-nginx-yum-centos6.sh | sh
 
+# 安装下载工具
+yum install -y wget
+
 # 下载 nginx 官方源
 wget -O /etc/yum.repos.d/nginx.repo https://github.com/mrhuangyuhui/nginx-practice/raw/master/nginx-centos6.repo
 
@@ -9,4 +12,4 @@ wget -O /etc/yum.repos.d/nginx.repo https://github.com/mrhuangyuhui/nginx-practi
 yum install -y nginx
 
 # 打印 nginx 版本信息
-nginx -V
+nginx -v
