b67ce2c24aaffba9aa2c145026d6788d7f395038
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-13 14:55:36 +0800

    Update install-nginx-yum-centos.sh

--- install-nginx-yum-centos.sh
+++ install-nginx-yum-centos.sh
@@ -2,10 +2,11 @@
 
 # nginx 源配置文件下载地址
 repo_url="https://github.com/mrhuangyuhui/nginx-practice/raw/master/"
+repo_file="/etc/yum.repos.d/nginx.repo"
 
-if [ "$#" -eq 1 ]
+if [ "${#}" -eq 1 ]
 then
-   case "$1" in
+   case "${1}" in
       "centos6")
          repo_url="${repo_url}nginx-centos6.repo"
          ;;
@@ -17,9 +18,13 @@ then
          exit 1
    esac
    
-   # 下载源配置文件
-   # wget -O /etc/yum.repos.d/nginx.repo $repo_url
-   echo "$repo_url"
+   if [ -e "${repo_file}" ]
+   then
+      echo "*** 源配置文件已存在，先删除。 ***"
+      rm -f "${repo_file}"
+   fi
+   echo "*** 下载源配置文件 ***"
+   wget -O "${repo_file}" "${repo_url}"
 fi
 
 # 安装 nginx
