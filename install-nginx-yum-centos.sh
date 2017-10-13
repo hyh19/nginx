bcb5a679be9a1957442b0febd6ae2f86c9d072ea
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-13 15:52:39 +0800

    Update install-nginx-yum-centos.sh

--- install-nginx-yum-centos.sh
+++ install-nginx-yum-centos.sh
@@ -4,31 +4,36 @@
 repo_url="https://github.com/mrhuangyuhui/nginx-practice/raw/master/"
 repo_file="/etc/yum.repos.d/nginx.repo"
 
+# 下载源配置文件
+function down_repo_file()
+{
+   # 如果源配置文件已经存在则删除
+   if [ -e "${repo_file}" ]
+   then
+      rm -f "${repo_file}"
+   fi
+   wget -O "${repo_file}" "${repo_url}"
+}
+
+# 打印脚本的使用方法
+function print_script_usage() {
+   echo "脚本的使用方法：./${0} centos6 或 ./${0} centos7"
+}
+
 if [ "${#}" -eq 1 ]
 then
    case "${1}" in
-      "centos6")
-         repo_url="${repo_url}nginx-centos6.repo"
-         ;;
-      "centos7")
-         repo_url="${repo_url}nginx-centos7.repo"
+      centos6 | centos7)
+         repo_url="${repo_url}nginx-${1}.repo"
+         down_repo_file
          ;;
       *)
          echo "*** 脚本参数错误 ***"
          ;;
    esac
    
-   # 判断源配置文件是否已经存在，如果已经存在则先删除再重新下载。
-   if [ -e "${repo_file}" ]
-   then
-      echo "*** 源配置文件已经存在，先删除。 ***"
-      rm -f "${repo_file}"
-   fi
-   echo "*** 下载源配置文件 ***"
-   wget -O "${repo_file}" "${repo_url}"
+   
 fi
-
-echo "运行脚本方法：./${0} centos6 或 ./${0} centos7，参数只能是 centos6 或 centos7"
 
 # 安装 nginx
 # yum install -y nginx
