20f01ddf7649d448dbf0c668e0a68dfe301a98ff
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2018-02-08 21:04:04 +0800

    Update install-nginx-source-yum.sh

--- install-nginx-source-yum.sh
+++ install-nginx-source-yum.sh
@@ -56,5 +56,82 @@ function install_dependencies_with_yum() {
 
 # 使用 apt 安装依赖
 function install_dependencies_with_apt() {
-    echo "Hello World"
+    echo ""
 }
+
+# 编译和安装源码
+function make_and_install() {
+    # 创建安装目录
+    mkdir -p $INSTALL_DIR
+    # 进入源码目录
+    cd $SOURCE_DIR
+
+    # 创建进程用户
+    useradd nginx -s /sbin/nologin -M
+    ./configure --user=nginx --group=nginx --prefix="${INSTALL_DIR}" --with-http_stub_status_module --with-http_ssl_module
+    make
+    make install
+}
+
+# 配置二进制文件路径
+function config_binary_path() {
+    echo "${INSTALL_ROOT}/${SOFTWARE_NAME}/bin" > $SOFTWARE_PROFILE
+}
+
+# 进入工作目录
+cd $WORKING_DIR
+
+# 下载判断发行版本的脚本
+rm -f $CHECK_SYS_SCRIPT_SAVE_PATH
+wget -O $CHECK_SYS_SCRIPT_SAVE_PATH $CHECK_SYS_SCRIPT_DOWNLOAD_URL
+
+if [ -e "$CHECK_SYS_SCRIPT_SAVE_PATH" ]; then
+    . $CHECK_SYS_SCRIPT_SAVE_PATH
+else
+    echo "[ERROR] Download ${CHECK_SYS_SCRIPT_NAME} failed."
+    exit 1
+fi
+
+# 安装依赖
+if CHECK_SYS_SCRIPT_SAVE_PATH "packageManager" "yum"; then
+    install_dependencies_with_yum
+elif CHECK_SYS_SCRIPT_SAVE_PATH "packageManager" "apt"; then
+    install_dependencies_with_apt
+else
+    echo "[ERROR] Not supported distro."
+    exit 1
+fi
+
+# 下载源码包
+if [ ! -e "$ARCHIVE_SAVE_PATH" ]; then
+    wget -O $ARCHIVE_SAVE_PATH $ARCHIVE_DOWNLOAD_URL
+fi
+
+# 下载失败，不再继续。
+if [ ! -e "$ARCHIVE_SAVE_PATH" ]; then
+    echo "[ERROR] Download ${ARCHIVE_NAME} failed."
+    exit 1
+fi
+
+# 备份旧的源码目录
+if [ -d "$SOURCE_DIR" ]; then
+    mv $SOURCE_DIR "${SOURCE_DIR}-$(date +%Y%m%d%H%M%S)"
+fi
+
+# 备份旧的安装目录
+if [ -d "$INSTALL_DIR" ]; then
+    mv $INSTALL_DIR "${INSTALL_DIR}-$(date +%Y%m%d%H%M%S)"
+fi
+
+# 解压源码包
+tar zxvf $ARCHIVE_SAVE_PATH
+
+# 开始编译和安装
+make_and_install
+
+# 配置二进制文件路径
+config_binary_path
+
+echo "################################################################################"
+echo "# Open a new terminal or enter: source ${SOFTWARE_PROFILE}"
+echo "################################################################################"
